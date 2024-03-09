fac7e846b1c93c01350c2145e4a23b11bc165a0d
Swofty <admin@swofty.net>
2024-03-09 14:45:50 +1100

    Changed skill level up message to be more accurate
    Took 10 minutes

--- generic/src/main/java/net/swofty/types/generic/event/actions/custom/skill/ActionSkillLevelUp.java
+++ generic/src/main/java/net/swofty/types/generic/event/actions/custom/skill/ActionSkillLevelUp.java
@@ -44,7 +44,7 @@ public class ActionSkillLevelUp extends SkyBlockEvent {
 
         player.sendMessage("§3§l▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
         player.sendMessage(Component.text("  §b§lSKILL LEVEL UP §3" + skillCategory + " §8" +
-                        (oldLevelDisplay.isEmpty() ? "0" : oldLevelDisplay) + ">§e" +
+                        (oldLevelDisplay.isEmpty() ? "§e" : oldLevelDisplay + ">§e") +
                         StringUtility.getAsRomanNumeral(newLevel))
                 .hoverEvent(Component.text("§eClick to view your " + skillCategory + " Skill progress!"))
                 .clickEvent(net.kyori.adventure.text.event.ClickEvent.runCommand("/viewskill " + skillCategory.toString().toUpperCase()))
