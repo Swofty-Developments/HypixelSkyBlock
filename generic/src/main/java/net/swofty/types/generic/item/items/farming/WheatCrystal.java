3793db258b4d285904d614c938afc1fab3dd4fdf
ItzKatze <136186750+ItzKatze@users.noreply.github.com>
2024-03-10 14:16:54 +0100

    Fixed wrong texture being used

--- generic/src/main/java/net/swofty/types/generic/item/items/farming/WheatCrystal.java
+++ generic/src/main/java/net/swofty/types/generic/item/items/farming/WheatCrystal.java
@@ -2,17 +2,17 @@ package net.swofty.types.generic.item.items.farming;
 
 import net.minestom.server.event.player.PlayerBlockPlaceEvent;
 import net.minestom.server.item.Material;
-import net.swofty.types.generic.entity.ServerOrbImpl;
+import net.swofty.types.generic.entity.ServerCrystalImpl;
 import net.swofty.types.generic.item.SkyBlockItem;
 import net.swofty.types.generic.item.impl.CustomSkyBlockItem;
 import net.swofty.types.generic.item.impl.Placeable;
-import net.swofty.types.generic.item.impl.ServerOrb;
+import net.swofty.types.generic.item.impl.ServerCrystal;
 import net.swofty.types.generic.item.impl.SkullHead;
 import net.swofty.types.generic.user.SkyBlockPlayer;
 import net.swofty.types.generic.user.statistics.ItemStatistics;
 import org.jetbrains.annotations.Nullable;
 
-public class FarmingOrb implements CustomSkyBlockItem, SkullHead, Placeable, ServerOrb {
+public class WheatCrystal implements CustomSkyBlockItem, SkullHead, Placeable, ServerCrystal {
     @Override
     public ItemStatistics getStatistics() {
             return ItemStatistics.EMPTY;
@@ -20,18 +20,18 @@ public class FarmingOrb implements CustomSkyBlockItem, SkullHead, Placeable, Ser
 
     @Override
     public String getSkullTexture(@Nullable SkyBlockPlayer player, SkyBlockItem item) {
-        return "557b354ce9bf8db0a7ed2afc79e27f28c3ccaaaf2c460a51993cac2b0564ef60";
+        return "9265f96f54b78885c46e7d2f86b1c1dbfe643c6060fc7fcc9834c3e3fd595135";
     }
 
     @Override
     public void onPlace(PlayerBlockPlaceEvent event, SkyBlockPlayer player, SkyBlockItem item) {
-        new ServerOrbImpl(Material.WHEAT_SEEDS, "557b354ce9bf8db0a7ed2afc79e27f28c3ccaaaf2c460a51993cac2b0564ef60").setInstance(
+        new ServerCrystalImpl(Material.WHEAT_SEEDS, "9265f96f54b78885c46e7d2f86b1c1dbfe643c6060fc7fcc9834c3e3fd595135").setInstance(
                 player.getInstance(), event.getBlockPosition().add(0, 1, 0)
         );
     }
 
     @Override
-    public Material getOrbSpawnMaterial() {
+    public Material getCrystalSpawnMaterial() {
         return Material.WHEAT_SEEDS;
     }
 }
