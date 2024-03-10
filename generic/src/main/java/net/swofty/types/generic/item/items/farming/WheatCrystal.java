ffa3db6024a63756db5dad18055637afc19faf55
ItzKatze <136186750+ItzKatze@users.noreply.github.com>
2024-03-10 14:36:45 +0100

    fix2

--- generic/src/main/java/net/swofty/types/generic/item/items/farming/WheatCrystal.java
+++ generic/src/main/java/net/swofty/types/generic/item/items/farming/WheatCrystal.java
@@ -2,17 +2,17 @@ package net.swofty.types.generic.item.items.farming;
 
 import net.minestom.server.event.player.PlayerBlockPlaceEvent;
 import net.minestom.server.item.Material;
-import net.swofty.types.generic.entity.ServerCrystalImpl;
+import net.swofty.types.generic.entity.ServerOrbImpl;
 import net.swofty.types.generic.item.SkyBlockItem;
 import net.swofty.types.generic.item.impl.CustomSkyBlockItem;
 import net.swofty.types.generic.item.impl.Placeable;
-import net.swofty.types.generic.item.impl.ServerCrystal;
+import net.swofty.types.generic.item.impl.ServerOrb;
 import net.swofty.types.generic.item.impl.SkullHead;
 import net.swofty.types.generic.user.SkyBlockPlayer;
 import net.swofty.types.generic.user.statistics.ItemStatistics;
 import org.jetbrains.annotations.Nullable;
 
-public class WheatCrystal implements CustomSkyBlockItem, SkullHead, Placeable, ServerCrystal {
+public class WheatCrystal implements CustomSkyBlockItem, SkullHead, Placeable, ServerOrb {
     @Override
     public ItemStatistics getStatistics() {
             return ItemStatistics.EMPTY;
@@ -25,13 +25,13 @@ public class WheatCrystal implements CustomSkyBlockItem, SkullHead, Placeable, S
 
     @Override
     public void onPlace(PlayerBlockPlaceEvent event, SkyBlockPlayer player, SkyBlockItem item) {
-        new ServerCrystalImpl(Material.WHEAT_SEEDS, "9265f96f54b78885c46e7d2f86b1c1dbfe643c6060fc7fcc9834c3e3fd595135").setInstance(
+        new ServerOrbImpl(Material.WHEAT_SEEDS, "9265f96f54b78885c46e7d2f86b1c1dbfe643c6060fc7fcc9834c3e3fd595135").setInstance(
                 player.getInstance(), event.getBlockPosition().add(0, 1, 0)
         );
     }
 
     @Override
-    public Material getCrystalSpawnMaterial() {
+    public Material getOrbSpawnMaterial() {
         return Material.WHEAT_SEEDS;
     }
 }
