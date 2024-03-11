eefe291e27e0697f2619c1731e51795e4dcca48d
ItzKatze <136186750+ItzKatze@users.noreply.github.com>
2024-03-11 17:26:16 +0100

    Added missing Crafting recipes

--- generic/src/main/java/net/swofty/types/generic/item/items/mining/crystal/gemstones/flawless/FlawlessSapphire.java
+++ generic/src/main/java/net/swofty/types/generic/item/items/mining/crystal/gemstones/flawless/FlawlessSapphire.java
@@ -2,13 +2,17 @@ package net.swofty.types.generic.item.items.mining.crystal.gemstones.flawless;
 
 import net.swofty.types.generic.gems.GemRarity;
 import net.swofty.types.generic.gems.Gemstone;
+import net.swofty.types.generic.item.ItemType;
 import net.swofty.types.generic.item.SkyBlockItem;
+import net.swofty.types.generic.item.impl.Craftable;
 import net.swofty.types.generic.item.impl.GemstoneImpl;
+import net.swofty.types.generic.item.impl.SkyBlockRecipe;
 import net.swofty.types.generic.item.impl.Unstackable;
+import net.swofty.types.generic.item.impl.recipes.ShapelessRecipe;
 import net.swofty.types.generic.user.SkyBlockPlayer;
 import org.jetbrains.annotations.Nullable;
 
-public class FlawlessSapphire implements GemstoneImpl, Unstackable {
+public class FlawlessSapphire implements GemstoneImpl, Unstackable, Craftable {
     @Override
     public String getSkullTexture(@Nullable SkyBlockPlayer player, SkyBlockItem item) {
         return "957cfa9c75ba584645ee2af6d9867d767ddea4667cdfc72dc1061dd1975ca7d0";
@@ -22,5 +26,16 @@ public class FlawlessSapphire implements GemstoneImpl, Unstackable {
     @Override
     public Gemstone getAssociatedGemstone() {
         return Gemstone.SAPPHIRE;
+    }
+
+    @Override
+    public SkyBlockRecipe<?> getRecipe() {
+        return new ShapelessRecipe(SkyBlockRecipe.RecipeType.MINING,
+                new SkyBlockItem(ItemType.FLAWLESS_SAPPHIRE_GEM), 1)
+                .add(ItemType.FINE_SAPPHIRE_GEM, 16)
+                .add(ItemType.FINE_SAPPHIRE_GEM, 16)
+                .add(ItemType.FINE_SAPPHIRE_GEM, 16)
+                .add(ItemType.FINE_SAPPHIRE_GEM, 16)
+                .add(ItemType.FINE_SAPPHIRE_GEM, 16);
     }
 }
