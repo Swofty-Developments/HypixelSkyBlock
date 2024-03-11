5335b2ffa0c693383cb10d5c47d4877ab7f21138
ItzKatze <136186750+ItzKatze@users.noreply.github.com>
2024-03-11 17:25:46 +0100

    Fixed Steve Skins

--- generic/src/main/java/net/swofty/types/generic/item/items/mining/crystal/gemstones/flawed/FlawedSapphire.java
+++ generic/src/main/java/net/swofty/types/generic/item/items/mining/crystal/gemstones/flawed/FlawedSapphire.java
@@ -1,11 +1,10 @@
 package net.swofty.types.generic.item.items.mining.crystal.gemstones.flawed;
 
+import net.swofty.types.generic.gems.GemRarity;
+import net.swofty.types.generic.gems.Gemstone;
 import net.swofty.types.generic.item.ItemType;
 import net.swofty.types.generic.item.SkyBlockItem;
-import net.swofty.types.generic.item.impl.Craftable;
-import net.swofty.types.generic.item.impl.CustomSkyBlockItem;
-import net.swofty.types.generic.item.impl.Sellable;
-import net.swofty.types.generic.item.impl.SkyBlockRecipe;
+import net.swofty.types.generic.item.impl.*;
 import net.swofty.types.generic.item.impl.recipes.ShapelessRecipe;
 import net.swofty.types.generic.user.SkyBlockPlayer;
 import net.swofty.types.generic.user.statistics.ItemStatistics;
@@ -14,10 +13,16 @@ import org.jetbrains.annotations.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class FlawedSapphire implements CustomSkyBlockItem, Sellable, Craftable {
+public class FlawedSapphire implements GemstoneImpl, Sellable, Craftable {
+
     @Override
-    public ItemStatistics getStatistics() {
-        return ItemStatistics.EMPTY;
+    public GemRarity getAssociatedGemRarity() {
+        return GemRarity.FLAWED;
+    }
+
+    @Override
+    public Gemstone getAssociatedGemstone() {
+        return Gemstone.SAPPHIRE;
     }
 
     @Override
@@ -27,18 +32,6 @@ public class FlawedSapphire implements CustomSkyBlockItem, Sellable, Craftable {
 
     public String getSkullTexture(@Nullable SkyBlockPlayer player, SkyBlockItem item) {
         return "8a0af99e8d8703194a847a55268cf5ef4ac4eb3b24c0ed86551339d10b646529";
-    }
-
-    @Override
-    public ArrayList<String> getLore(SkyBlockPlayer player, SkyBlockItem item) {
-        return new ArrayList<>(Arrays.asList(
-                "§7A slightly better version of",
-                "§7§bSapphire§7, but it could still",
-                "§7use some work.",
-                "",
-                "§7§7Some say that when §eharnessed",
-                "§eproperly§7, it can give its",
-                "§7owner extra §b✎ Intelligence§7."));
     }
 
     @Override
