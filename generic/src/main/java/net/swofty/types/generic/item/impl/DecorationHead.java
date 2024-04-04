23c3c84f30fda6c83c093b9f9524b1137862368c
ItzKatze <136186750+ItzKatze@users.noreply.github.com>
2024-04-04 21:16:53 +0200

    made decoration heads placeable

---
+++ generic/src/main/java/net/swofty/types/generic/item/impl/DecorationHead.java
@@ -0,0 +1,13 @@
+package net.swofty.types.generic.item.impl;
+
+import net.minestom.server.event.player.PlayerBlockPlaceEvent;
+import net.swofty.types.generic.item.SkyBlockItem;
+import net.swofty.types.generic.user.SkyBlockPlayer;
+
+public interface DecorationHead extends SkullHead {
+
+    @Override
+    default void onPlace(PlayerBlockPlaceEvent event, SkyBlockPlayer player, SkyBlockItem item) {
+        event.setCancelled(false);
+    }
+}
