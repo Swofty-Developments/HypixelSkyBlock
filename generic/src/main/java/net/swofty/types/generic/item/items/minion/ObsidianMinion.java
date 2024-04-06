f5e7f60eddb2e1a8d7dfe6255b0f3a207fae0a7e
Epicportal14 <jlsagasa145@gmail.com>
2024-04-06 12:16:17 +0530

    Completely added all Mining Minion And Fixed TimeBetweenAction!

---
+++ generic/src/main/java/net/swofty/types/generic/item/items/minion/ObsidianMinion.java
@@ -0,0 +1,29 @@
+package net.swofty.types.generic.item.items.minion;
+
+import net.swofty.types.generic.item.ItemType;
+import net.swofty.types.generic.item.impl.CustomSkyBlockItem;
+import net.swofty.types.generic.item.impl.Minion;
+import net.swofty.types.generic.minion.MinionRegistry;
+
+public class ObsidianMinion implements CustomSkyBlockItem, Minion {
+    @Override
+    public MinionRegistry getMinionRegistry() {
+        return MinionRegistry.OBSIDIAN;
+    }
+
+    @Override
+    public ItemType getBaseCraftMaterial() {
+        return ItemType.OBSIDIAN;
+    }
+
+    @Override
+    public ItemType getEnchantedCraftMaterial() {
+        return ItemType.ENCHANTED_OBSIDIAN;
+    }
+
+    @Override
+    public ItemType getFirstBaseItem() {
+        return ItemType.DIAMOND_PICKAXE;
+    }
+
+}
