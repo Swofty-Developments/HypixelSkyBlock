258f0901d3cc7bb6e90b6509067812f985148775
CodRiSFR <rg7191913@gmail.com>
2024-04-10 22:37:24 +0530

    Now player dont lose coins on island and added more minion fuel

--- generic/src/main/java/net/swofty/types/generic/item/items/enchanted/EnchantedBread.java
+++ generic/src/main/java/net/swofty/types/generic/item/items/enchanted/EnchantedBread.java
@@ -2,13 +2,14 @@ package net.swofty.types.generic.item.items.enchanted;
 
 import net.swofty.types.generic.item.SkyBlockItem;
 import net.swofty.types.generic.item.impl.Enchanted;
+import net.swofty.types.generic.item.impl.MinionFuelItem;
 import net.swofty.types.generic.item.impl.Sellable;
 import net.swofty.types.generic.user.SkyBlockPlayer;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class EnchantedBread implements Enchanted, Sellable {
+public class EnchantedBread implements Enchanted, Sellable, MinionFuelItem {
 
     @Override
     public double getSellValue() {
@@ -21,5 +22,15 @@ public class EnchantedBread implements Enchanted, Sellable {
                 "§7increases the speed of",
                 "§7your minion by §a5%§7",
                 "§7for 12 hours§7!"));
+    }
+
+    @Override
+    public double getMinionFuelPercentage() {
+        return 5;
+    }
+
+    @Override
+    public long getFuelLastTimeInMS() {
+        return 43200000; // 12 Hours
     }
 }
