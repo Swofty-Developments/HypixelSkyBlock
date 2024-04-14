993b9674416dc878ce283cc5cc710a6c81fcf2d7
Jesús <63926389+DcRubik@users.noreply.github.com>
2024-04-14 13:57:08 +0200

    Update VillagerMathsEnjoyer.java

--- type-village/src/main/java/net/swofty/type/village/villagers/VillagerMathsEnjoyer.java
+++ type-village/src/main/java/net/swofty/type/village/villagers/VillagerMathsEnjoyer.java
@@ -1,11 +1,16 @@
 package net.swofty.type.village.villagers;
 
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
 import net.minestom.server.entity.metadata.villager.VillagerMeta;
+import net.swofty.types.generic.entity.villager.NPCVillagerDialogue;
 import net.swofty.types.generic.entity.villager.NPCVillagerParameters;
 import net.swofty.types.generic.entity.villager.SkyBlockVillagerNPC;
 
+import java.util.stream.Stream;
+
 public class VillagerMathsEnjoyer extends SkyBlockVillagerNPC {
+
     public VillagerMathsEnjoyer() {
         super(new NPCVillagerParameters() {
             @Override
@@ -31,7 +36,34 @@ public class VillagerMathsEnjoyer extends SkyBlockVillagerNPC {
     }
 
     @Override
-    public void onClick(PlayerClickVillagerNPCEvent e) {
-        e.player().sendMessage("§cThis Feature is not there yet. §aOpen a Pull request at https://github.com/Swofty-Developments/HypixelSkyBlock to get it added quickly!");
+    public void onClick(PlayerClickVillagerNPCEvent event) {
+        Player player = event.player();
+        if (isInDialogue(player)) return;
+
+        setDialogue(player);
+    }
+
+    private boolean isInDialogue(Player player) {
+        return false;
+    }
+
+    private void setDialogue(Player player) {
+        player.sendMessage("Hello! This is the dialogue for the Maths Enjoyer NPC.");
+        player.sendMessage("The formula to Magical Power is:");
+        player.sendMessage("Stats Mult. = 29.97 * (ln(0.0019 * MP + 1))^1.2");
+        player.sendMessage("Have fun with that!");
+    }
+
+    @Override
+    public NPCVillagerDialogue.DialogueSet[] getDialogueSets() {
+        return Stream.of(
+                NPCVillagerDialogue.DialogueSet.builder()
+                        .key("hello").lines(new String[]{
+                                "§e[NPC] Maths Enjoyer: §fHey if you really want to know...",
+                                "§e[NPC] Maths Enjoyer: The formula to §6Magical Power §fis...",
+                                "§e[NPC] Maths Enjoyer: §dStats Mult. §f= §b29.97§e(§aln(§b0.0019§6MP§a+1)§e)^§b1.2",
+                                "§e[NPC] Maths Enjoyer: Have fun with that!"
+                        }).build()
+        ).toArray(NPCVillagerDialogue.DialogueSet[]::new);
     }
 }
