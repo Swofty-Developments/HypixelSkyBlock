55014513a7991019705bb69511013cd31983b395
TreesOnTop <85085371+TreesOnTop@users.noreply.github.com>
2024-04-20 15:33:47 -0400

    add semour and fix some item stats

--- generic/src/main/java/net/swofty/types/generic/item/set/ArmorSetRegistry.java
+++ generic/src/main/java/net/swofty/types/generic/item/set/ArmorSetRegistry.java
@@ -3,13 +3,15 @@ package net.swofty.types.generic.item.set;
 import lombok.Getter;
 import net.swofty.types.generic.item.ItemType;
 import net.swofty.types.generic.item.set.impl.ArmorSet;
-import net.swofty.types.generic.item.set.sets.LeafletSet;
-import net.swofty.types.generic.item.set.sets.MinerOutfitSet;
+import net.swofty.types.generic.item.set.sets.*;
 
 @Getter
 public enum ArmorSetRegistry {
     LEAFLET(LeafletSet.class, ItemType.LEAFLET_SANDALS, ItemType.LEAFLET_PANTS, ItemType.LEAFLET_TUNIC, ItemType.LEAFLET_HAT),
     MINOR_OUTFIT(MinerOutfitSet.class, ItemType.MINERS_OUTFIT_BOOTS, ItemType.MINERS_OUTFIT_LEGGINGS, ItemType.MINERS_OUTFIT_CHESTPLATE, ItemType.MINERS_OUTFIT_HELMET),
+    CHEAP_TUXEDO(CheapTuxedoSet.class, ItemType.CHEAP_TUXEDO_OXFORDS, ItemType.CHEAP_TUXEDO_PANTS, ItemType.CHEAP_TUXEDO_JACKET, null),
+    FANCY_TUXEDO(FancyTuxedoSet.class, ItemType.FANCY_TUXEDO_OXFORDS, ItemType.FANCY_TUXEDO_PANTS, ItemType.FANCY_TUXEDO_JACKET, null),
+    ELEGANT_TUXEDO(ElegantTuxedoSet.class, ItemType.ELEGANT_TUXEDO_OXFORDS, ItemType.ELEGANT_TUXEDO_PANTS, ItemType.ELEGANT_TUXEDO_JACKET, null)
     ;
 
     private final Class<? extends ArmorSet> clazz;
