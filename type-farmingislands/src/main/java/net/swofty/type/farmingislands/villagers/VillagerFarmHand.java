3d0b74e02fa7da61dd46046057e1bd1ed7288668
CodRiSFR <rg7191913@gmail.com>
2024-04-23 11:38:07 +0530

    fixes

--- type-farmingislands/src/main/java/net/swofty/type/farmingislands/villagers/VillagerFarmHand.java
+++ type-farmingislands/src/main/java/net/swofty/type/farmingislands/villagers/VillagerFarmHand.java
@@ -40,7 +40,7 @@ public class VillagerFarmHand extends NPCVillagerDialogue {
     public NPCVillagerDialogue.DialogueSet[] getDialogueSets() {
         return List.of(
                 DialogueSet.builder()
-                        .key("initial-hello").lines(new String[]{
+                        .key("quest-hello").lines(new String[]{
                                 "§e[NPC] Farmhand§f: Welcome to the Barn!",
                                 "§e[NPC] Farmhand§f: Feel free to harvest any crops on the island, I'm taking a break.",
                                 "§e[NPC] Farmhand§f: Did you craft a §aWhat Minion §fyet? Here's the recipe!"
@@ -50,6 +50,13 @@ public class VillagerFarmHand extends NPCVillagerDialogue {
                                 "§e[NPC] Farmhand§f: Nice! I have a Tier X Wheat Minion of my own, maybe one day you can be like me!",
                                 "§e[NPC] Farmhand§f: For now here's some §eEnchanted Bread §fto get you started.",
                                 "§e[NPC] Farmhand§f: Enchanted Bread can be used in Farming Minion as fuel! It makes them temporarily faster."
+                        }).build(),
+                DialogueSet.builder()
+                        .key("initial-hello").lines(new String[]{
+                                "§e[NPC] Farmhand§f: The farm animals here provide a variety or resources.",
+                                "§e[NPC] Farmhand§f: Did you know that chicken drops eggs and cows be milked?",
+                                "§e[NPC] Farmhand§f: Collect wheat, carrots, potatoes, pumpkins and melon slices to add to your Collection and unlock cool rewards!",
+                                "§e[NPC] Farmhand§f: Those §dFairy Souls scare off the animals."
                         }).build()
         ).stream().toArray(NPCVillagerDialogue.DialogueSet[]::new);
     }
@@ -60,20 +67,21 @@ public class VillagerFarmHand extends NPCVillagerDialogue {
         MissionData data = e.player().getMissionData();
 
         if (data.isCurrentlyActive(MissionTalkToFarmHand.class)) {
-            setDialogue(e.player(), "initial-hello").thenRun(() -> {
+            setDialogue(e.player(), "quest-hello").thenRun(() -> {
                 data.endMission(MissionTalkToFarmHand.class);
             });
             return;
         }
-        if (data.hasCompleted(MissionCraftWheatMinion.class)) {
-            data.endMission(MissionCraftWheatMinion.class);
+        if (!data.hasCompleted(MissionCraftWheatMinion.class)) {
+            e.player().sendMessage("§e[NPC] Farmhand§f: Craft a §aWheat Minion§f!");
             return;
         }
-        if (data.hasCompleted(MissionTalkToFarmhandAgain.class)) {
+        if (!data.hasCompleted(MissionTalkToFarmhandAgain.class)) {
             setDialogue(e.player(), "spoke-again").thenRun(() -> {
                 data.endMission(MissionTalkToFarmhandAgain.class);
             });
             return;
         }
+        setDialogue(e.player(), "initial-hello");
     }
 }
