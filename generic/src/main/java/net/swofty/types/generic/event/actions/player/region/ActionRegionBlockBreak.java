ac8efe47c4d159bc2149949b56f32929dfcd2285
ItzKatze <136186750+ItzKatze@users.noreply.github.com>
2024-05-09 20:05:46 +0200

    added multiple block drop support

--- generic/src/main/java/net/swofty/types/generic/event/actions/player/region/ActionRegionBlockBreak.java
+++ generic/src/main/java/net/swofty/types/generic/event/actions/player/region/ActionRegionBlockBreak.java
@@ -15,9 +15,11 @@ import net.swofty.types.generic.item.SkyBlockItem;
 import net.swofty.types.generic.region.RegionType;
 import net.swofty.types.generic.region.SkyBlockMiningConfiguration;
 import net.swofty.types.generic.region.SkyBlockRegion;
+import net.swofty.types.generic.region.mining.MineableBlock;
 import net.swofty.types.generic.user.SkyBlockPlayer;
 import net.swofty.types.generic.event.SkyBlockEvent;
 import net.swofty.types.generic.event.custom.CustomBlockBreakEvent;
+import net.swofty.types.generic.utility.MaterialQuantifiableRandom;
 
 public class ActionRegionBlockBreak implements SkyBlockEventClass {
 
@@ -67,24 +69,31 @@ public class ActionRegionBlockBreak implements SkyBlockEventClass {
                     player, item.getMaterial(), event.getBlockPosition()
             ));
 
+            MineableBlock mineableBlock = MineableBlock.get(block);
+            Double fortune = player.getStatistics().allStatistics().getOverall(mineableBlock.getFortuneType());
+            Double dropMultiplicator = (1 + (fortune*0.01));
+            Integer drops = mineableBlock.getDrops().getAmount(dropMultiplicator);
+
             /**
              * Handle block dropping
              */
-            DroppedItemEntityImpl droppedItem = new DroppedItemEntityImpl(item, player);
-            Pos pos = Pos.fromPoint(event.getBlockPosition());
-            // Move the dropped item to the center of the block
-            pos = pos.add(0.5, 0.5, 0.5);
-            // Move block closer to player by 0.5 blocks
-            Pos playerPos = player.getPosition().add(0, 0.5, 0);
-            double x = playerPos.x() - pos.x();
-            double y = playerPos.y() - pos.y();
-            double z = playerPos.z() - pos.z();
-            double distance = Math.sqrt(x * x + y * y + z * z);
-            double multiplier = 1.4 / distance;
-            pos = pos.add(x * multiplier, y * multiplier * 3, z * multiplier);
+            for (int i = 0; i < drops; i++) {
+                DroppedItemEntityImpl droppedItem = new DroppedItemEntityImpl(item, player);
+                Pos pos = Pos.fromPoint(event.getBlockPosition());
+                // Move the dropped item to the center of the block
+                pos = pos.add(0.5, 0.5, 0.5);
+                // Move block closer to player by 0.5 blocks
+                Pos playerPos = player.getPosition().add(0, 0.5, 0);
+                double x = playerPos.x() - pos.x();
+                double y = playerPos.y() - pos.y();
+                double z = playerPos.z() - pos.z();
+                double distance = Math.sqrt(x * x + y * y + z * z);
+                double multiplier = 1.4 / distance;
+                pos = pos.add(x * multiplier, y * multiplier * 3, z * multiplier);
 
-            droppedItem.setInstance(player.getInstance(), pos);
-            droppedItem.addViewer(player);
+                droppedItem.setInstance(player.getInstance(), pos);
+                droppedItem.addViewer(player);
+            }
         }
         shouldItemDrop = false;
     }
