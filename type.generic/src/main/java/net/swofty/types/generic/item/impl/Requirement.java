bd88175090ad3d6f939a09692014da53ddb6b3b5
CodRiSFR <rg7191913@gmail.com>
2024-08-28 23:01:56 +0530

    compilation error

--- type.generic/src/main/java/net/swofty/types/generic/item/impl/Requirement.java
+++ type.generic/src/main/java/net/swofty/types/generic/item/impl/Requirement.java
@@ -1,8 +1,9 @@
 package net.swofty.types.generic.item.impl;
 
+import net.swofty.types.generic.item.SkyBlockItem;
 import net.swofty.types.generic.user.SkyBlockPlayer;
 
 public interface Requirement {
-    boolean isMet(SkyBlockPlayer player, Item item);
+    boolean isMet(SkyBlockPlayer player, SkyBlockItem item);
     String getErrorMessage(); // Return the error message if the requirement is not met.
 }
