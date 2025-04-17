01e93c9c0b7104efb9ca4eb9926bb1abca0b662b
Hamza <kinghamza09088@gmail.com>
2025-04-17 23:03:54 +0500

    Update polar to 1.14.0

--- type.generic/build.gradle.kts
+++ type.generic/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("net.minestom:minestom-snapshots:1_21_4-7599413490") {
         exclude(group = "org.jboss.shrinkwrap.resolver", module = "shrinkwrap-resolver-depchain")
     }
-    implementation("dev.hollowcube:polar:1.12.1")
+    implementation("dev.hollowcube:polar:1.14.0")
     implementation("org.yaml:snakeyaml:2.2")
 }
 
