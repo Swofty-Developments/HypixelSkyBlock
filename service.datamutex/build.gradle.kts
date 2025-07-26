31910a4065a119f6b9afb4c9c78ea69ddd8a80ee
Swofty <admin@swofty.net>
2025-07-26 21:27:53 +1000

    chore: add build.gradle.kts for datamutex service
    Took 54 seconds

---
+++ service.datamutex/build.gradle.kts
@@ -0,0 +1,40 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    java
+    application
+    id("io.github.goooler.shadow") version "8.1.7"
+}
+
+group = "net.swofty"
+version = "3.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    maven("https://jitpack.io")
+}
+
+dependencies {
+    implementation(project(":service.generic"))
+    implementation(project(":commons"))
+    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+}
+
+application {
+    mainClass.set("net.swofty.service.datamutex.DataMutexService")
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveBaseName.set("ServiceDataMutex")
+        archiveClassifier.set("")
+        archiveVersion.set("")
+    }
+}
