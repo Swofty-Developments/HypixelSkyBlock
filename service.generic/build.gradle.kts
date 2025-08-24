e198be8fdff95151b93f7c2c84d02e111763bb9f
AriDev <75741608+ArikSquad@users.noreply.github.com>
2025-08-24 14:57:11 +0300

    fix: invalid adventure-api version

--- service.generic/build.gradle.kts
+++ service.generic/build.gradle.kts
@@ -22,6 +22,6 @@ dependencies {
     implementation(project(":commons"))
     implementation("com.github.Swofty-Developments:AtlasRedisAPI:1.1.3")
     // implementation("net.swofty:AtlasRedisAPI:1.1.4")
-    implementation("net.kyori:adventure-api:4.25.0")
-    implementation("net.kyori:adventure-text-serializer-gson:4.25.0")
+    implementation("net.kyori:adventure-api:4.24.0")
+    implementation("net.kyori:adventure-text-serializer-gson:4.24.0")
 }
