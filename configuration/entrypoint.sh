f9d7141666dd88413bdfcf2b6be9587596320f67
Aris <64918822+Arisamiga@users.noreply.github.com>
2025-10-26 18:02:05 +0000

    Link Updates and Fixes to reflect new version

--- configuration/entrypoint.sh
+++ configuration/entrypoint.sh
@@ -11,6 +11,9 @@ mv ./configuration/resources.json.tmp ./configuration/resources.json
 # Replace the secret in settings.yml
 sed -i "s/secret: '.*'/secret: '$secret'/" ./settings.yml
 
+# Set the settings.yml bind: ip: 'localhost' to bind: ip: '0.0.0.0'
+sed -i "s/ip: 'localhost'/ip: '0.0.0.0'/" ./settings.yml
+
 
 # Clear any previously created Screens (Starting freshhh)
 screen -wipe
@@ -20,12 +23,14 @@ screen -dmS HypixelCore_ISLAND java --enable-preview -jar HypixelCore.jar SKYBLO
 sleep 20
 screen -dmS HypixelCore_HUB java --enable-preview -jar HypixelCore.jar SKYBLOCK_HUB
 screen -dmS HypixelCore_FARMING java --enable-preview -jar HypixelCore.jar SKYBLOCK_THE_FARMING_ISLANDS
-screen -dmS NanoLimbo java -jar NanoLimbo-1.9.1-all.jar
+screen -dmS PROTOTYPE_LOBBY java --enable-preview -jar HypixelCore.jar PROTOTYPE_LOBBY
+screen -dmS BEDWARS_LOBBY java --enable-preview -jar HypixelCore.jar BEDWARS_LOBBY
+screen -dmS NanoLimbo java -jar NanoLimbo-1.9.8.jar
 screen -dmS ServiceAPI java -jar ServiceAPI.jar
 screen -dmS ServiceAuctionHouse java -jar ServiceAuctionHouse.jar
 screen -dmS ServiceBazaar java -jar ServiceBazaar.jar
 screen -dmS ServiceItemTracker java -jar ServiceItemTracker.jar
-
+screen -dmS ServiceParty java -jar ServiceParty.jar
 echo "Started all services with a total of: $(screen -ls | grep -c 'Detached') screens"
 
 # Keep the container running
--- configuration/mongo-init.sh
+++ configuration/mongo-init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Importing regions..."
-mongoimport --db Minestom --collection regions --type csv --ignoreBlanks --headerline --file /csv/Minestom.regions.csv || echo "Failed to import regions"
+mongoimport --db Minestom --collection regions --type csv --ignoreBlanks --headerline --file /csv/skyblock/Minestom.regions.csv || echo "Failed to import regions"
 
 echo "Importing fairysouls..."
-mongoimport --db Minestom --collection fairysouls --type csv --ignoreBlanks --headerline --file /csv/Minestom.fairysouls.csv || echo "Failed to import fairysouls"
+mongoimport --db Minestom --collection fairysouls --type csv --ignoreBlanks --headerline --file /csv/skyblock/Minestom.fairysouls.csv || echo "Failed to import fairysouls"
 
 echo "Importing crystals..."
-mongoimport --db Minestom --collection crystals --type csv --ignoreBlanks --headerline --file /csv/Minestom.crystals.csv || echo "Failed to import crystals"
+mongoimport --db Minestom --collection crystals --type csv --ignoreBlanks --headerline --file /csv/skyblock/Minestom.crystals.csv || echo "Failed to import crystals"
