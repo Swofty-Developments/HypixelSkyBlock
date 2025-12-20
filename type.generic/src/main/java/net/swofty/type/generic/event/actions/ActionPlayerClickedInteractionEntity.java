dd0b0fd1a0d208139796878b63b48a614e9ec302
AriDev <75741608+ArikSquad@users.noreply.github.com>
2025-12-20 19:08:12 +0200

    refactor: move InteractionEntity listener to generic

--- type.generic/src/main/java/net/swofty/type/generic/event/actions/ActionPlayerClickedInteractionEntity.java
+++ type.generic/src/main/java/net/swofty/type/generic/event/actions/ActionPlayerClickedInteractionEntity.java
@@ -1,4 +1,4 @@
-package net.swofty.type.skyblockgeneric.event.actions.entity;
+package net.swofty.type.generic.event.actions;
 
 import net.minestom.server.entity.PlayerHand;
 import net.minestom.server.event.player.PlayerEntityInteractEvent;
@@ -6,13 +6,13 @@ import net.swofty.type.generic.entity.InteractionEntity;
 import net.swofty.type.generic.event.EventNodes;
 import net.swofty.type.generic.event.HypixelEvent;
 import net.swofty.type.generic.event.HypixelEventClass;
-import net.swofty.type.skyblockgeneric.user.SkyBlockPlayer;
+import net.swofty.type.generic.user.HypixelPlayer;
 
-public class ActionPlayerClickedGlassDisplay implements HypixelEventClass {
+public class ActionPlayerClickedInteractionEntity implements HypixelEventClass {
 
 	@HypixelEvent(node = EventNodes.PLAYER, requireDataLoaded = true)
 	public void run(PlayerEntityInteractEvent event) {
-		final SkyBlockPlayer player = (SkyBlockPlayer) event.getPlayer();
+		final HypixelPlayer player = (HypixelPlayer) event.getPlayer();
 
 		if (event.getHand() != PlayerHand.MAIN) return;
 		if (event.getTarget() instanceof InteractionEntity interaction) {
