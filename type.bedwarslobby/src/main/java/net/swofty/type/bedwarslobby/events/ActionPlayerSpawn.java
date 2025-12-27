ab03cfec0a8589b9a527e2f28cbecf9e0e9c1dc5
petethepossum <47347759+petethepossum@users.noreply.github.com>
2025-12-28 01:52:33 +1300

    Force Survival on bedwars lobby join .(Forgot gamemode import)

--- type.bedwarslobby/src/main/java/net/swofty/type/bedwarslobby/events/ActionPlayerSpawn.java
+++ type.bedwarslobby/src/main/java/net/swofty/type/bedwarslobby/events/ActionPlayerSpawn.java
@@ -7,6 +7,7 @@ import net.swofty.type.generic.event.EventNodes;
 import net.swofty.type.generic.event.HypixelEvent;
 import net.swofty.type.generic.event.HypixelEventClass;
 import net.swofty.type.generic.user.HypixelPlayer;
+import net.minestom.server.entity.GameMode;
 
 public class ActionPlayerSpawn implements HypixelEventClass {
 
