ca673afeb3e4f99454ec3890b1d0fab2ec993383
ArikSquad <75741608+ArikSquad@users.noreply.github.com>
2025-12-28 19:15:18 +0200

    feat: more proper treasure hoarder

--- type.skyblockgeneric/src/main/java/net/swofty/type/skyblockgeneric/entity/mob/mobs/dwarvenmines/MobTreasureHoarder.java
+++ type.skyblockgeneric/src/main/java/net/swofty/type/skyblockgeneric/entity/mob/mobs/dwarvenmines/MobTreasureHoarder.java
@@ -1,16 +1,27 @@
 package net.swofty.type.skyblockgeneric.entity.mob.mobs.dwarvenmines;
 
+import lombok.NonNull;
 import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.ai.GoalSelector;
 import net.minestom.server.entity.ai.TargetSelector;
+import net.minestom.server.entity.ai.target.LastEntityDamagerTarget;
+import net.minestom.server.utils.time.TimeUnit;
+import net.swofty.commons.skyblock.item.ItemType;
+import net.swofty.commons.skyblock.statistics.ItemStatistic;
 import net.swofty.commons.skyblock.statistics.ItemStatistics;
 import net.swofty.type.generic.gui.inventory.item.GUIMaterial;
 import net.swofty.type.skyblockgeneric.entity.mob.BestiaryMob;
 import net.swofty.type.skyblockgeneric.entity.mob.MobType;
+import net.swofty.type.skyblockgeneric.entity.mob.ai.ClosestEntityRegionTarget;
+import net.swofty.type.skyblockgeneric.entity.mob.ai.MeleeAttackWithinRegionGoal;
+import net.swofty.type.skyblockgeneric.entity.mob.ai.RandomRegionStrollGoal;
 import net.swofty.type.skyblockgeneric.entity.mob.impl.RegionPopulator;
 import net.swofty.type.skyblockgeneric.loottable.OtherLoot;
 import net.swofty.type.skyblockgeneric.loottable.SkyBlockLootTable;
+import net.swofty.type.skyblockgeneric.region.RegionType;
 import net.swofty.type.skyblockgeneric.skill.SkillCategories;
+import net.swofty.type.skyblockgeneric.user.SkyBlockPlayer;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
@@ -23,12 +34,12 @@ public class MobTreasureHoarder extends BestiaryMob implements RegionPopulator {
 
 	@Override
 	public int getMaxBestiaryTier() {
-		return 0;
+		return 15;
 	}
 
 	@Override
 	public int getBestiaryBracket() {
-		return 0;
+		return 4;
 	}
 
 	@Override
@@ -48,51 +59,83 @@ public class MobTreasureHoarder extends BestiaryMob implements RegionPopulator {
 
 	@Override
 	public Integer getLevel() {
-		return 0;
+		return 70;
 	}
 
 	@Override
 	public List<GoalSelector> getGoalSelectors() {
-		return List.of();
+		return List.of(
+				new MeleeAttackWithinRegionGoal(this,
+						1.6,
+						20,
+						TimeUnit.SERVER_TICK,
+						RegionType.UPPER_MINES),
+				new RandomRegionStrollGoal(this, 15, RegionType.UPPER_MINES)
+		);
 	}
 
 	@Override
 	public List<TargetSelector> getTargetSelectors() {
-		return List.of();
+		return List.of(
+				new LastEntityDamagerTarget(this, 16),
+				new ClosestEntityRegionTarget(this,
+						6,
+						entity -> entity instanceof SkyBlockPlayer,
+						RegionType.UPPER_MINES)
+		);
 	}
 
 	@Override
 	public ItemStatistics getBaseStatistics() {
-		return null;
+		return ItemStatistics.builder()
+				.withBase(ItemStatistic.HEALTH, 15000D)
+				.withBase(ItemStatistic.DAMAGE, 500D)
+				.withBase(ItemStatistic.SPEED, 100D)
+				.build();
 	}
+
 
 	@Override
 	public @Nullable SkyBlockLootTable getLootTable() {
-		return null;
+		return new SkyBlockLootTable() {
+			@Override
+			public @NonNull List<LootRecord> getLootTable() {
+				return List.of(
+						new LootRecord(ItemType.STARFALL, 1, 100),
+						new LootRecord(ItemType.STARFALL, 1, 50),
+						new LootRecord(ItemType.TREASURITE, 1, 0.5)
+				);
+			}
+
+			@Override
+			public @NotNull CalculationMode getCalculationMode() {
+				return CalculationMode.CALCULATE_INDIVIDUAL;
+			}
+		};
 	}
 
 	@Override
 	public SkillCategories getSkillCategory() {
-		return null;
+		return SkillCategories.COMBAT;
 	}
 
 	@Override
 	public long damageCooldown() {
-		return 0;
+		return 500;
 	}
 
 	@Override
 	public OtherLoot getOtherLoot() {
-		return null;
+		return new OtherLoot(70, 0, 0);
 	}
 
 	@Override
 	public List<MobType> getMobTypes() {
-		return List.of();
+		return List.of(MobType.HUMANOID);
 	}
 
 	@Override
 	public List<Populator> getPopulators() {
-		return List.of();
+		return List.of(new Populator(RegionType.UPPER_MINES, 20));
 	}
 }
