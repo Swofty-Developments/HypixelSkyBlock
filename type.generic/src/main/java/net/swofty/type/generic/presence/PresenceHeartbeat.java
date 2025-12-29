e46398f41dd2ff503d79782601baec64275f6344
petethepossum <47347759+petethepossum@users.noreply.github.com>
2025-12-29 19:39:42 +1300

    fix ttl task

--- type.generic/src/main/java/net/swofty/type/generic/presence/PresenceHeartbeat.java
+++ type.generic/src/main/java/net/swofty/type/generic/presence/PresenceHeartbeat.java
@@ -1,11 +1,13 @@
 package net.swofty.type.generic.presence;
 
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.timer.TaskSchedule;
 import net.swofty.commons.ServiceType;
 import net.swofty.commons.presence.PresenceInfo;
 import net.swofty.commons.protocol.objects.presence.UpdatePresenceProtocolObject;
 import net.swofty.proxyapi.redis.ServerOutboundMessage;
 import net.swofty.type.generic.HypixelConst;
+import net.swofty.type.generic.HypixelGenericLoader;
 import net.swofty.type.generic.user.HypixelPlayer;
 
 /**
@@ -16,8 +18,8 @@ public final class PresenceHeartbeat {
 
     public static void start() {
         MinecraftServer.getSchedulerManager().buildTask(PresenceHeartbeat::pulse)
-                .delay(20, net.minestom.server.timer.SchedulerManager.TimeUnit.SECOND)
-                .repeat(45, net.minestom.server.timer.SchedulerManager.TimeUnit.SECOND)
+                .delay(TaskSchedule.seconds(20))
+                .repeat(TaskSchedule.seconds(45))
                 .schedule();
     }
 
@@ -28,7 +30,7 @@ public final class PresenceHeartbeat {
         } catch (Exception ignored) {
         }
 
-        for (HypixelPlayer player : HypixelConst.getLoadedPlayers()) {
+        for (HypixelPlayer player : HypixelGenericLoader.getLoadedPlayers()) {
             PresenceInfo info = new PresenceInfo(
                     player.getUuid(),
                     true,
