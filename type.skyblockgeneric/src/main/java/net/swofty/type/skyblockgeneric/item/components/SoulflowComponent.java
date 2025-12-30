2fbd88ff4e984174a3df3d2643463a523c1552fb
ArikSquad <75741608+ArikSquad@users.noreply.github.com>
2025-12-30 17:40:25 +0200

    feat: move lore to SoulflowComponent

--- type.skyblockgeneric/src/main/java/net/swofty/type/skyblockgeneric/item/components/SoulflowComponent.java
+++ type.skyblockgeneric/src/main/java/net/swofty/type/skyblockgeneric/item/components/SoulflowComponent.java
@@ -4,12 +4,20 @@ import lombok.Getter;
 import net.swofty.type.skyblockgeneric.gui.inventories.GUIConsumeSoulflow;
 import net.swofty.type.skyblockgeneric.item.SkyBlockItemComponent;
 
+import java.util.List;
+
 public class SoulflowComponent extends SkyBlockItemComponent {
 
 	@Getter
 	private final int amount;
 
 	public SoulflowComponent(int amount) {
+		addInheritedComponent(new LoreUpdateComponent(
+				List.of(
+						"§7Hold and right-click to consume,",
+						"§7gaining §3+" + amount + "⸎ Soulflow§7."
+				), false
+		));
 		addInheritedComponent(
 				new InteractableComponent((player, item) -> {
 					new GUIConsumeSoulflow(item).open(player);
