962ed5fef7a2a832faf075a1dc70cccc3cc4fb14
Swofty <admin@swofty.net>
2026-01-09 22:35:21 +1100

    fix: fix jerry

--- type.island/src/main/java/net/swofty/type/island/events/custom/ActionIslandSaveJerry.java
+++ type.island/src/main/java/net/swofty/type/island/events/custom/ActionIslandSaveJerry.java
@@ -1,5 +1,6 @@
 package net.swofty.type.island.events.custom;
 
+import net.swofty.type.generic.entity.hologram.ServerHolograms;
 import net.swofty.type.generic.event.EventNodes;
 import net.swofty.type.generic.event.HypixelEvent;
 import net.swofty.type.generic.event.HypixelEventClass;
@@ -11,6 +12,17 @@ public class ActionIslandSaveJerry implements HypixelEventClass {
     @HypixelEvent(node = EventNodes.CUSTOM, requireDataLoaded = false)
     public void run(IslandSavedIntoDatabaseEvent event) {
         JerryInformation jerryInformation = event.getIsland().getJerryInformation();
+
+        if (jerryInformation == null) return;
+
+        if (jerryInformation.getJerry() != null) {
+            jerryInformation.getJerry().remove();
+        }
+        if (jerryInformation.getHologram() != null) {
+            ServerHolograms.removeExternalHologram(jerryInformation.getHologram());
+        }
+
+        if (jerryInformation.getJerryPosition() == null) return;
 
         event.getIsland().getDatabase().insertOrUpdate("jerry_position_x", jerryInformation.getJerryPosition().x());
         event.getIsland().getDatabase().insertOrUpdate("jerry_position_y", jerryInformation.getJerryPosition().y());
