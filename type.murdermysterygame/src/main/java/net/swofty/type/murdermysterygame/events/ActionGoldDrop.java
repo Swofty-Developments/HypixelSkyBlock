c28fb7fb1bc340ed4944c9b2e89de8f06edbf6d4
deyoyk <karthikw7755@gmail.com>
2026-01-11 01:31:27 +0530

    fixing: hunger and gold dropping

--- type.murdermysterygame/src/main/java/net/swofty/type/murdermysterygame/events/ActionGoldDrop.java
+++ type.murdermysterygame/src/main/java/net/swofty/type/murdermysterygame/events/ActionGoldDrop.java
@@ -1,6 +1,5 @@
 package net.swofty.type.murdermysterygame.events;
 
-import net.minestom.server.entity.ItemEntity;
 import net.minestom.server.event.item.ItemDropEvent;
 import net.minestom.server.item.Material;
 import net.swofty.type.murdermysterygame.TypeMurderMysteryGameLoader;
@@ -10,8 +9,6 @@ import net.swofty.type.murdermysterygame.user.MurderMysteryPlayer;
 import net.swofty.type.generic.event.EventNodes;
 import net.swofty.type.generic.event.HypixelEvent;
 import net.swofty.type.generic.event.HypixelEventClass;
-
-import java.time.Duration;
 
 public class ActionGoldDrop implements HypixelEventClass {
 
@@ -29,17 +26,8 @@ public class ActionGoldDrop implements HypixelEventClass {
 
         Material material = event.getItemStack().material();
 
-        // Allow gold dropping - create shared ItemEntity that anyone can pick up
         if (material == Material.GOLD_INGOT) {
-            ItemEntity itemEntity = new ItemEntity(event.getItemStack());
-            itemEntity.setInstance(
-                    player.getInstance(),
-                    player.getPosition().add(0, player.getEyeHeight(), 0)
-            );
-            itemEntity.setVelocity(
-                    player.getPosition().add(0, 0.3, 0).direction().mul(6)
-            );
-            itemEntity.setPickupDelay(Duration.ofMillis(500));
+            event.setCancelled(true);
             return;
         }
 
