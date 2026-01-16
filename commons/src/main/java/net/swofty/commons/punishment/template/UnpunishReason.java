7bfa8c8c1f35e7e81be489c553872bed7d89896e
ArikSquad <75741608+ArikSquad@users.noreply.github.com>
2026-01-16 23:40:13 +0200

    refactor: remove requiredRank

--- commons/src/main/java/net/swofty/commons/punishment/template/UnpunishReason.java
+++ commons/src/main/java/net/swofty/commons/punishment/template/UnpunishReason.java
@@ -4,24 +4,22 @@ import lombok.Getter;
 
 @Getter
 public enum UnpunishReason {
-    WRONG_PLAYER("Wrong Player", null, null),
-    WRONG_PUNISHMENT("Wrong Punishment", null, null),
-    INSUFFICIENT_EVIDENCE("Insufficient Evidence", "IE", null),
-    ACCOUNT_SECURED("Account Secured", null, "MODERATOR"),
-    SECOND_CHANCE("Second Chance", "SC", "ADMIN"),
-    CHARGEBACK_APPEALED("Chargeback Appealed", null, "ADMIN"),
-    CHARGEBACK_FAILED("Chargeback Failed", null, "ADMIN"),
-    CHARGEBACK_CANCELLED("Chargeback Cancelled", null, "ADMIN"),
-    NOT_FRAUD("Not Fraud", null, "ADMIN"),
-    TIME_SERVED_MAINTAIN_WEIGHT("Time Served (Maintain Weight)", "MW", "ADMIN");
+    WRONG_PLAYER("Wrong Player", null),
+    WRONG_PUNISHMENT("Wrong Punishment", null),
+    INSUFFICIENT_EVIDENCE("Insufficient Evidence", "IE"),
+    ACCOUNT_SECURED("Account Secured", null),
+    SECOND_CHANCE("Second Chance", "SC"),
+    CHARGEBACK_APPEALED("Chargeback Appealed", null),
+    CHARGEBACK_FAILED("Chargeback Failed", null),
+    CHARGEBACK_CANCELLED("Chargeback Cancelled", null),
+    NOT_FRAUD("Not Fraud", null),
+    TIME_SERVED_MAINTAIN_WEIGHT("Time Served (Maintain Weight)", "MW");
 
     private final String reason;
     private final String tag;
-    private final String requiredRank;
 
-    UnpunishReason(String reason, String tag, String requiredRank) {
+    UnpunishReason(String reason, String tag) {
         this.reason = reason;
         this.tag = tag;
-        this.requiredRank = requiredRank;
     }
 }
