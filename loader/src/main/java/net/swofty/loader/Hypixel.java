c511cb3030c2a162d486aab579fbeebec84fec09
Swofty <admin@swofty.net>
2026-02-08 14:19:15 +1100

    fix(loader): increase proxy connection timeout to 5 seconds

--- loader/src/main/java/net/swofty/loader/Hypixel.java
+++ loader/src/main/java/net/swofty/loader/Hypixel.java
@@ -228,7 +228,7 @@ public class Hypixel {
             }
         });
 
-        CompletableFuture.delayedExecutor(100, TimeUnit.MILLISECONDS)
+        CompletableFuture.delayedExecutor(5, TimeUnit.SECONDS)
                 .execute(() -> {
                     if (startServer.isDone()) return;
                     Logger.error("Couldn't connect to proxy. Shutting down...");
