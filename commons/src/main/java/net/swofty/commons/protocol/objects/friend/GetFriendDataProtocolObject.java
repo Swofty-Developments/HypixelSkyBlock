62fc40d77c852149fc1e36946f55e8352e767483
Jacob Nardella (Swofty) <admin@swofty.net>
2026-04-19 20:12:00 +1000

    refactor: migrate GetFriendDataProtocolObject to JacksonSerializer + records

--- commons/src/main/java/net/swofty/commons/protocol/objects/friend/GetFriendDataProtocolObject.java
+++ commons/src/main/java/net/swofty/commons/protocol/objects/friend/GetFriendDataProtocolObject.java
@@ -1,9 +1,9 @@
 package net.swofty.commons.protocol.objects.friend;
 
 import net.swofty.commons.friend.FriendData;
+import net.swofty.commons.protocol.JacksonSerializer;
 import net.swofty.commons.protocol.ProtocolObject;
 import net.swofty.commons.protocol.Serializer;
-import org.json.JSONObject;
 
 import java.util.UUID;
 
@@ -13,57 +13,12 @@ public class GetFriendDataProtocolObject extends ProtocolObject
 
     @Override
     public Serializer<GetFriendDataMessage> getSerializer() {
-        return new Serializer<>() {
-            @Override
-            public String serialize(GetFriendDataMessage value) {
-                JSONObject json = new JSONObject();
-                json.put("playerUuid", value.playerUuid.toString());
-                return json.toString();
-            }
-
-            @Override
-            public GetFriendDataMessage deserialize(String json) {
-                JSONObject jsonObject = new JSONObject(json);
-                UUID playerUuid = UUID.fromString(jsonObject.getString("playerUuid"));
-                return new GetFriendDataMessage(playerUuid);
-            }
-
-            @Override
-            public GetFriendDataMessage clone(GetFriendDataMessage value) {
-                return new GetFriendDataMessage(value.playerUuid);
-            }
-        };
+        return new JacksonSerializer<>(GetFriendDataMessage.class);
     }
 
     @Override
     public Serializer<GetFriendDataResponse> getReturnSerializer() {
-        return new Serializer<>() {
-            @Override
-            public String serialize(GetFriendDataResponse value) {
-                JSONObject json = new JSONObject();
-                if (value.friendData != null) {
-                    json.put("friendData", value.friendData.getSerializer().serialize(value.friendData));
-                } else {
-                    json.put("friendData", JSONObject.NULL);
-                }
-                return json.toString();
-            }
-
-            @Override
-            public GetFriendDataResponse deserialize(String json) {
-                JSONObject jsonObject = new JSONObject(json);
-                if (jsonObject.isNull("friendData")) {
-                    return new GetFriendDataResponse(null);
-                }
-                FriendData friendData = FriendData.getStaticSerializer().deserialize(jsonObject.getString("friendData"));
-                return new GetFriendDataResponse(friendData);
-            }
-
-            @Override
-            public GetFriendDataResponse clone(GetFriendDataResponse value) {
-                return new GetFriendDataResponse(value.friendData);
-            }
-        };
+        return new JacksonSerializer<>(GetFriendDataResponse.class);
     }
 
     public record GetFriendDataMessage(UUID playerUuid) {
